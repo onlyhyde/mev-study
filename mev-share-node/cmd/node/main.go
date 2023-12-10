80b7112eed62e57ca136a6a3ddf330147303aad9
0xTopaz <kevin.dev201@gmail.com>
2023-12-10 14:25:05 +0900

    feat : add flag

--- mev-share-node/cmd/node/main.go
+++ mev-share-node/cmd/node/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	version = "dev" // is set during build process
+
+	// Default values
+	defaultDebug                 = os.Getenv("DEBUG") == "1"
+	defaultLogProd               = os.Getenv("LOG_PROD") == "1"
+
+	// Flags
+	debugPtr                 = flag.Bool("debug", defaultDebug, "print debug output")
+	logProdPtr               = flag.Bool("log-prod", defaultLogProd, "log in production mode (json)")
+)
 
 func main() {
+	flag.Parse()
 	fmt.Println("Hello, World!")
+	fmt.Println("Version:", version)
+	fmt.Println("Debug:", *debugPtr)
+	fmt.Println("LogProd:", *logProdPtr)
+
+	// 환경설정값 읽기
+	args := os.Args[1:]
+	err := godotenv.Load(args[0] + ".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	connectionsClosed := make(chan struct{})
+	go func() {
+		close(connectionsClosed)
+		fmt.Println("Connections closed")
+	}()
+
+	<-connectionsClosed
+}
+
+func getConfig() {
+	
 }
--- mev-share-node/go.mod
+++ mev-share-node/go.mod
@@ -1,3 +1,5 @@
 module github.com/onlyhyde/mev-study/mev-share-node
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1
